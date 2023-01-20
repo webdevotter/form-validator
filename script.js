bce4345f385114f073db9caa78c1a57d0ddf5205
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-20 07:10:54 -0600

    Check Length, Email & Password Match

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ function showSuccess(input) {
 }
 
 //Check email is valid
-function checkEmail(email) {
+function checkEmail(input.value) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(email.value.trim())) {
-    showSuccess(email);
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
   } else {
-    showError(email, 'Email is not valid');
+    showError(input, 'Email is not valid');
   }
 }
 
@@ -39,6 +39,24 @@ function checkRequired(inputArr) {
   });
 }
 
+//Check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if(input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if(input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  }
+}
+
 //Get field name
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -49,5 +67,9 @@ form.addEventListener('submit', function(e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
  
 });
